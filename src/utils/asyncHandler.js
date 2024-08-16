e04e73207843065921952c2cdcf2693dc6b6cd4e
abhishek-waghmare-mars <abhishek.waghmare@effem.com>
2024-08-16 15:50:56 +0530

    util code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler)=>{
+  (req,res,next) => {
+      Promise.resolve(requestHandler(req,res,next)).
+      catch((err)=>next(err))
+  }
+}
+
+
+
+export {asyncHandler}
+
+
+
+// asyncHandler with try catch block
+
+// const asyncHandler = (fn) => async (req,res,next) =>{
+//   try{
+
+//   }
+//   catch(err){
+//     res.status(err.code || 500).json({
+//       success :false,
+//       message:err.message
+//     })
+//   }
+// }
