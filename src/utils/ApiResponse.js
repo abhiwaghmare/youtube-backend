769001054c70111c028c9dbfa7d175898e0c9cfe
abhishek-waghmare-mars <a1999waghmare@gmail.com>
2024-08-18 14:22:14 +0530

    add multer and cloudinary files for file upload

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,10 @@
-class ApiResponse{
-  constructor(statusCode,data,messsage="Success"){
-    this.statusCode - statusCode
-    this.data = data
-    this.messsage = messsage
-    this.success = statusCode
+class ApiResponse {
+  constructor(statusCode, data, messsage = "Success") {
+    this.statusCode - statusCode;
+    this.data = data;
+    this.messsage = messsage;
+    this.success = statusCode;
   }
 }
+
+export { ApiResponse };
